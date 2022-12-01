f4c6d5236f4bf0fc6afd445931ca1aeeb5e6f5ca
Owen Burns <owenburns88@gmail.com>
2022-11-30 20:39:59 -0500

    fixed link prediction testing script

--- scripts/run_link_pred_all.sh
+++ scripts/run_link_pred_all.sh
@@ -2,8 +2,9 @@
 
 dataset=$1
 custom=$2
+method=$3
 for d in ./$dataset/tasks/*/; do
     [ -L "${d%/}" ] && continue
     echo "$d"
-    ./link_prediction_eval.sh $d $dataset
+    ./link_prediction_eval.sh $d $dataset $method
 done
